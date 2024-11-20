a799b708faddc7da4131e3542541f17bc74eff5d
Jordan Smalls <116747817+Jordan-Smalls@users.noreply.github.com>
2024-11-20 13:05:53 -0500

    Added dark/light toggle and copywrite in footer

--- controller.js
+++ controller.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
+
+  var body = $("body");
   var darkmode_btn = $("#darkmode-toggle");
+  var darkmode_toggled = true;
+
+
   darkmode_btn.click(function() {
-    $("body").css("background-color","black");
+    if (!darkmode_toggled) {
+	body.css({"background-color":"#f2f2f2", "color":"#333333"});
+	darkmode_btn.css({"background-color":"#333333", "color":"#f2f2f2"});
+	darkmode_btn.html('');
+	darkmode_toggled = true;
+    }
+    else {
+	body.css({"background-color":"#333333", "color":"#f2f2f2"});
+	darkmode_btn.css({"background-color":"#f2f2f2", "color":"#333333"});
+	darkmode_btn.html('');
+	darkmode_toggled = false;
+    }
   });
+
 });
