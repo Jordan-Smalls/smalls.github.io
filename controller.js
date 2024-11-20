634c5781ba14f293bdd0be6fe1985119902e08da
Jordan Smalls <116747817+Jordan-Smalls@users.noreply.github.com>
2024-11-20 11:42:56 +0000

    Update controller.js

--- controller.js
+++ controller.js
@@ -2,7 +2,6 @@ var darkmode_btn = $("#darkmode-toggle");
 
 $(document).ready(function() {
   darkmode_btn.click(function() {
-    alert("jquery working");
     $("body").css("background-color","black");
   });
 });
