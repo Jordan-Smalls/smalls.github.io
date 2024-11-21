744ce6e581cd9281a033feafc26eaf3d24595f12
Jordan Smalls <116747817+Jordan-Smalls@users.noreply.github.com>
2024-11-21 13:55:05 -0500

    More Changes
    - Nav buttons added
    - Content sections added
    - Page formatting
    - It's lookin' pretty

--- controller.js
+++ controller.js
@@ -1,9 +1,60 @@
 $(document).ready(function() {
 
+  // CONSTANTS
+  const FOUR_TENTH_SEC = 400;
+  const HALF_SEC = 500;
+
+  // ELEMENTS
   var body = $("body");
+  var main_view = $("#main_view");
+  var about_section = $("#about_section");
+  var edu_section = $("#edu_section");
+  var work_section = $("#work_hist_section");
+  var projects_section = $("#projects_section");
+
+  // BUTTONS
   var darkmode_btn = $("#darkmode-toggle");
   var darkmode_toggled = true;
+  var about_btn = $("#about_btn");
+  var edu_btn = $("#edu_btn");
+  var work_btn = $("#work_btn");
+  var projects_btn = $("#projects_btn");
 
+  // INIT FUNCTIONS
+  main_view.hide();
+  about_section.show();
+  edu_section.hide();
+  work_section.hide();
+  projects_section.hide();
+  main_view.fadeIn(HALF_SEC);
+
+  about_btn.click(function() {
+	about_section.fadeIn(FOUR_TENTH_SEC);
+  	edu_section.hide();
+  	work_section.hide();
+  	projects_section.hide();
+  });
+
+  edu_btn.click(function() {
+	about_section.hide();
+  	edu_section.fadeIn(FOUR_TENTH_SEC);
+  	work_section.hide();
+  	projects_section.hide();
+  });
+
+  work_btn.click(function() {
+	about_section.hide();
+  	edu_section.hide();
+  	work_section.fadeIn(FOUR_TENTH_SEC);
+  	projects_section.hide();
+  });
+
+  projects_btn.click(function() {
+	about_section.hide();
+  	edu_section.hide();
+  	work_section.hide();
+  	projects_section.fadeIn(FOUR_TENTH_SEC);
+  });
 
   darkmode_btn.click(function() {
     if (!darkmode_toggled) {
